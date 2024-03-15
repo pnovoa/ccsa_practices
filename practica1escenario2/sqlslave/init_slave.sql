4c172037ea5c44eaf0a1388a606077fb8279080b
pnovoa <pavel.novoa@gmail.com>
2024-03-15 20:12:54 +0100

    mariadb replica success

---
+++ practica1escenario2/sqlslave/init_slave.sql
@@ -0,0 +1,10 @@
+STOP SLAVE;
+
+CHANGE MASTER TO
+  MASTER_HOST='mariadb_master',
+  MASTER_USER='replication_user',
+  MASTER_PASSWORD='bigs3cret',
+  MASTER_PORT=3306,
+  MASTER_CONNECT_RETRY=10;
+  
+START SLAVE;
